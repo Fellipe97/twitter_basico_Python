708920ba5baa3f84a6e023f84da526ef81adb212
Fellipe97 <lfalmeida97@hotmail.com>
2021-09-28 02:57:11 -0300

    save

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ DEBUG = True      #podemos alterar o arquivo e ele atualiza sozinho, sem precisa
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'  # URI de conexao com o banco de dados,ou seja, o caminho que passar até o banco de dados (estabelecer a conexão)
 SQLALCHEMY_TRACK_MODIFICATIONS = True    #acabar com o warming
+
+SECRET_KEY = 'um-nome-bem-seguro'
